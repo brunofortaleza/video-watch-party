b7fc41654d08e10edcdb24a76f31df714503bcae
Bruno Fortaleza <brunosilvapalmas@gmail.com>
2023-05-19 15:33:43 -0300

    feat: added personalization for messages info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def flash_messages
+    flash_messages = []
+
+    flash.each do |key, value|
+      if key == 'notice'
+        flash_messages << "<div class='alert alert-success alert-dismissible fade show' role='alert'>#{value}<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>"
+      elsif key == 'alert'
+        flash_messages << "<div class='alert alert-danger alert-dismissible fade show' role='alert'>#{value}<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>"
+      end
+    end
+
+    flash_messages.join.html_safe
+  end
 end
