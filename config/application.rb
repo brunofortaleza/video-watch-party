f70fd826fe111d33bf003967b2352a5c7b592a49
Bruno Fortaleza <brunosilvapalmas@gmail.com>
2023-05-19 17:47:36 -0300

    test: added test generation by rspec

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,14 @@ module VideoWatchParty
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :'pt-BR'
+
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+    end
   end
 end
